fa8e69eda5416dcb29ba6d28b7cb1308b120d706
liwia <liwia.topolska@gmail.com>
2017-12-17 19:06:57 +0100

    replacing Square component into function

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+function Square (props) {
+    return
         return (
             <button className="square" onClick={()=>this.props.onClick()}>
             {this.props.value}
             </button>
     );
-    }
 }
 
 class Board extends React.Component {
