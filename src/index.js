4478749fe3e9b204bc259f8e05a7aecd155c6a66
liwia <liwia.topolska@gmail.com>
2017-12-17 19:41:10 +0100

    changes in handleClick method

--- src/index.js
+++ src/index.js
@@ -20,13 +20,17 @@ class Board extends React.Component {
     }
 
     handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            squares: squares,
+            history: history.concat([{
+                squares: squares,
+            }]),
             xIsNext: !this.state.xIsNext,
         });
     }
